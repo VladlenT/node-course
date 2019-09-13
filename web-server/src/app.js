b41cd7f31556cca050f99656b1b9a873414ba2e3
Vladlen Tereshchenko <vladlen1995@gmail.com>
2019-09-13 15:55:28 +0300

    added hanblebars support and some template partials using it

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,15 +1,18 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
 // Define paths for Express config
 const publicDirPath = path.join(__dirname, '../public/');
-const viewsPath = path.join(__dirname, '../templates');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to use
 app.use(express.static(publicDirPath));
@@ -17,13 +20,14 @@ app.use(express.static(publicDirPath));
 app.get('/', (req, res) => {
   res.render('index.hbs', {
     title: 'Weather',
-    name: 'Vladlen',
+    author: 'Vladlen Tereshchenko',
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     title: 'About',
+    author: 'Vladlen Tereshchenko',
   });
 });
 
@@ -31,6 +35,7 @@ app.get('/help', (req, res) => {
   res.render('help.hbs', {
     title: 'Help page',
     message: 'Welcome to the help page',
+    author: 'Vladlen Tereshchenko',
   });
 });
 
