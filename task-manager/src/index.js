eb6761a9d2b09949b6fd6b747f8ab8a8182c1906
Vladlen Tereshchenko <vladlen1995@gmail.com>
2019-09-18 14:40:27 +0300

    finished CRUD for task-management API

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,25 +1,16 @@
 const express = require('express');
 require('./db/mongoose');
-const User = require('./db/models/user');
+const Task = require('./models/task');
+const userRouter = require('./routers/user');
+const taskRouter = require('./routers/task');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-app.post('/users', (req, res) => {
-  const user = new User(req.body);
-
-  user
-    .save()
-    .then(user => {
-      res.send(user);
-    })
-    .catch(e => {
-      res.status(400).send(e);
-    });
-});
+app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(port, () => {
-  console.log(`App is up on http://localhost:3000`);
+  console.log(`App is up on http://localhost:${port}`);
 });
