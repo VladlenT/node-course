01d8529a98164f748c8b91320cafdc80fe77dd06
Vladlen Tereshchenko <vladlent.dev@gmail.com>
2020-05-30 17:34:20 +0300

    added some websockets functions

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,1 +1,29 @@
-io();
+const socket = io();
+const form = document.querySelector('form');
+const locationBtn = document.querySelector('#send-location');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const messageInput = form.elements.namedItem('message');
+
+  socket.emit('sendMessage', messageInput.value, acknowledgement => {
+    console.log(acknowledgement);
+  });
+
+  messageInput.value = '';
+});
+
+socket.on('message', message => {
+  console.log(message);
+});
+
+locationBtn.addEventListener('click', () => {
+  if (!navigator.geolocation) return alert('Geolocation is not supported by your browser');
+
+  navigator.geolocation.getCurrentPosition(position => {
+    const { latitude, longitude } = position.coords;
+    socket.emit('sendLocation', { latitude, longitude }, () => {
+      console.log('Location shared!');
+    });
+  });
+});
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,8 +1,35 @@
 const port = process.env.PORT || 3000;
 const { server, io } = require('./app');
+const Filter = require('bad-words');
 
-io.on('connection', () => {
+io.on('connection', socket => {
   console.log('new WebSockets connection');
+
+  socket.emit('message', 'Welcome!');
+  socket.broadcast.emit('message', 'A new user has joined');
+
+  socket.on('sendMessage', (message, callback) => {
+    const filter = new Filter();
+
+    if (filter.isProfane(message)) {
+      return callback('Rejected. Profanity is not allowed!');
+    }
+
+    io.emit('message', message);
+    callback('Delivered!');
+  });
+
+  socket.on('sendLocation', (coords, acknowledgement) => {
+    io.emit(
+      'message',
+      `Location: https://google.com/maps?q=${coords.latitude},${coords.longitude}`,
+    );
+    acknowledgement();
+  });
+
+  socket.on('disconnect', () => {
+    io.emit('message', 'User has left');
+  });
 });
 
 server.listen(port, () => {
